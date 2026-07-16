57b821ad61d2bf973f9017dd4f39fa74f6a74ba2
ashutosh raj <ashutoshraj9065@gamil.com>
2026-07-16 16:55:46 +0530

    pyhton revision

---
+++ pythonpractise.py
@@ -0,0 +1,18 @@
+numbers = [10, 20, 30, 40, 50]
+# count=0
+# sum=0
+largest=numbers[0]
+
+# for i in numbers:
+#     count=count+1
+# print(count)
+
+# for i in numbers:
+#     sum=sum+i
+# print(sum)
+ 
+for i in numbers:
+    if i>largest:
+        largest=i
+
+print(largest)
